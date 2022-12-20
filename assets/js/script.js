8a043e28b0dcca0030013d45ecec8257cb7f3b06
Stelian2022 <stanciu.stelian86@yahoo.fr>
2022-12-20 15:40:13 +0100

    kkk

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+console.log("connect")
+//boucle for 
+//1 je definis un index et donne son point de depart let i=0
+//2 je definis le  point d'arret de ma boucle
+//3 je definis m'incrementation de ma boucle
+for(let i=0;i<10;i++){
+   console.log("bonjour") 
+//document.write("bonjour!<br>");
+}
+
+//bouble while
+
+//let j=0;
+//while(j<10){
+  //  console.log(j);
+    //j++;
+//}
+let j=1;
+while(j<11){
+    console.log(j);
+    j++;
+}
+
+//boucle foreach => array
+
+const user = ["Bob","Peter","Carlos","Patrick"];
+user.forEach(function(element){
+   console.log(element);
+   
+})
+
+
+
